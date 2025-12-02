858ecea77abb1ff32cbe7ebf24ed26c5bee9861d
Eldar Abdivaitov <eldarmeta@outlook.com>
2025-12-02 12:48:45 -0500

    feat: exercise 1 - display all products using JDBC

--- src/main/java/org/pluralsight/Main.java
+++ src/main/java/org/pluralsight/Main.java
@@ -1,18 +1,31 @@
 package org.pluralsight;
 
-import javax.sql.DataSource;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class Main {
     public static void main(String[] args) {
 
-        try {
-            DataSource dataSource = DataSourceFactory.getDataSource();
-            Connection connection = dataSource.getConnection();
-            System.out.println("CONNECTED TO MYSQL SUCCESSFULLY!");
-            connection.close();
-        }
-        catch (Exception e) {
+        String sql = "SELECT ProductID, ProductName, UnitPrice FROM products";
+
+        try (
+                Connection connection = DataSourceFactory.getDataSource().getConnection();
+                PreparedStatement statement = connection.prepareStatement(sql);
+                ResultSet results = statement.executeQuery()
+        ) {
+            System.out.println("PRODUCTS LIST:");
+            System.out.println("--------------------------------");
+
+            while (results.next()) {
+                int id = results.getInt("ProductID");
+                String name = results.getString("ProductName");
+                double price = results.getDouble("UnitPrice");
+
+                System.out.printf("%d | %s | $%.2f%n", id, name, price);
+            }
+
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
